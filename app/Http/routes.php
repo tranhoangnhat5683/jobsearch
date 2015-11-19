68db5732d27cb67a75fbd8fc7b381d23da96d9cb
Just4Fun <tranhoangnhat5683@gmail.com>
2015-11-20 02:08:54 +0700

    add api router

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,10 +10,17 @@
   | and give it the controller to call when that URI is requested.
   |
  */
-
-Route::get('/', 'WelcomeController@index');
-
-Route::get('home', 'HomeController@index');
+//Danh sach cac Router chinh ve UI
+Route::get('/', 'HomeController@index');
+Route::get('/list', 'HomeController@search');
+Route::get('/profile', 'HomeController@profile');
+Route::get('/api/user/search', 'Api\UserController@search');
+Route::get('/api/page/search', 'Api\PageController@search');
+Route::get('/api/skill/search', 'Api\SkillController@search');
+Route::get('/api/Character/search', 'Api\CharacterController@search');
+//Danh sach ve API o day
+Route::controller('api', 'ApiController');
+//Test va thu nghiem nen dc remove
 Route::get('angular','TestController@angular');
 Route::get('neo','TestController@neo');
 Route::get('cypher','TestController@cypher');
