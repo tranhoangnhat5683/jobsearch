7ee7df2719e4f935a7152e0104d28c3d7bd17054
cuongduong257 <duongthaicuong@gmail.com>
2015-11-21 13:28:40 +0700

    Fix Source
    Fix Source

--- resources/views/includes/js.blade.php
+++ resources/views/includes/js.blade.php
@@ -6,24 +6,12 @@
 <script src="assets/admin/layout/scripts/layout.js" type="text/javascript"></script>
 
 <!--Tu trang profile-->
-<script src="assets/global/plugins/jquery-migrate.min.js" type="text/javascript"></script>
-<script src="assets/global/plugins/jquery-ui/jquery-ui.min.js" type="text/javascript"></script>
-<script src="assets/global/plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js" type="text/javascript"></script>
 <script src="assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
 <script src="assets/global/plugins/jquery.blockui.min.js" type="text/javascript"></script>
-<script src="assets/global/plugins/jquery.cokie.min.js" type="text/javascript"></script>
-<script src="assets/global/plugins/uniform/jquery.uniform.min.js" type="text/javascript"></script>
-<script src="assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js" type="text/javascript"></script>
 
-<script src="assets/global/plugins/bootstrap-fileinput/bootstrap-fileinput.js" type="text/javascript"></script>
-<script src="assets/global/plugins/jquery.sparkline.min.js" type="text/javascript"></script>
 <script src="assets/admin/layout/scripts/quick-sidebar.js" type="text/javascript"></script>
 <script src="assets/admin/layout/scripts/demo.js" type="text/javascript"></script>
 <script src="assets/admin/pages/scripts/profile.js" type="text/javascript"></script>
 
 
 <!--Tu trang list -->    
-<script src="assets/global/plugins/amcharts/amcharts/amcharts.js" type="text/javascript"></script>
-<script src="assets/global/plugins/amcharts/amcharts/themes/light.js" type="text/javascript"></script>
-<script src="assets/global/plugins/amcharts/amcharts/radar.js" type="text/javascript"></script>
-<script src="assets/admin/pages/scripts/charts-amcharts.js"></script>
