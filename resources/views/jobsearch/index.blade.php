33ac8044ae1085e278a84539cd88cb53c85ea4cd
cuongduong257 <duongthaicuong@gmail.com>
2015-11-21 15:40:21 +0700

    Update UI
    Update UI

--- resources/views/jobsearch/index.blade.php
+++ resources/views/jobsearch/index.blade.php
@@ -12,38 +12,20 @@
 					<div class="form-content">
 						<form class="" action="" method="get" accept-charset="utf-8">
 							<div class="row">
-								<div class="form-group col-md-6">
+								<div class="form-group col-md-12">
 									<label>Characteristics</label>
-									<input type="text" class="form-control input-lg" placeholder="Nhập tính cách cần tìm...">
+									<input type="text" class="form-control input-lg" placeholder="..." autofocus>
 								</div> 
+								
+							</div>
+							<div class="row">
 								<div class="form-group col-md-6">
 									<label>Skill</label>
 									<input type="text" class="form-control input-lg" id="input-skill" multiple="multiple" placeholder="Nhập tính cách cần tìm...">
 								</div> 
-								<!-- <div class="form-group col-md-12">
-									<button type="button" class="btn btn-primary" data-toggle="button" aria-pressed="false">
-										Careful </button>
-									<button type="button" class="btn btn-default active" data-toggle="button" aria-pressed="true">
-												Active </button>
-									<button type="button" class="btn red active" data-toggle="button" aria-pressed="true">
-												Open </button>
-									<button type="button" class="btn blue active" data-toggle="button" aria-pressed="true">
-												Teamwork </button>
-								</div> -->
-							</div>
-							<div class="row">
-								<div class="form-group col-md-6">
-									<label>Categories</label>
-									<select class="form-control js-example-basic-multiple" id="input-category">
-										<option>Công Nghệ Thông tin</option>
-										<option>Accounting</option>
-										<option>Arts/Design</option>
-										<option>Architecture/Interior Design</option>
-									</select>
-								</div>
 								<div class="form-group col-md-6">
 									<label>Location</label>
-									<select class="form-control js-example-basic-multiple" id="input-location">
+									<select class="form-control js-example-basic-multiple" id="input-location" multiple="multiple">
 										<option>Hồ Chí Minh</option>
 										<option>Hà Nội</option>
 										<option>Phú Yên</option>
@@ -53,78 +35,6 @@
 								</div>
 
 							</div>
-							<div class="row">
-								<div class="form-group col-sm-6 form-md-line-input">
-									<label class=" control-label" for="form_control_1">Gender</label>
-									<div class="">
-										<div class="md-radio-inline">
-											<div class="md-radio">
-												<input type="radio" id="radio53" name="gender" class="md-radiobtn" checked="">
-												<label for="radio53">
-												<span class="inc"></span>
-												<span class="check"></span>
-												<span class="box"></span>
-												All </label>
-											</div>
-											<div class="md-radio">
-												<input type="radio" id="radio54" name="gender" class="md-radiobtn" >
-												<label for="radio54">
-												<span></span>
-												<span class="check"></span>
-												<span class="box"></span>
-												Male </label>
-											</div>
-											<div class="md-radio">
-												<input type="radio" id="radio55" name="gender" class="md-radiobtn">
-												<label for="radio55">
-												<span></span>
-												<span class="check"></span>
-												<span class="box"></span>
-												Female </label>
-											</div>
-										</div>
-									</div>
-								</div>
-								<div class="form-group col-sm-6 form-md-line-input">
-									<label class=" control-label" for="form_control_2">Level</label>
-									<div class="">
-										<div class="md-radio-inline">
-											<div class="md-radio">
-												<input type="radio" id="radio59" name="level" class="md-radiobtn">
-												<label for="radio59">
-												<span></span>
-												<span class="check"></span>
-												<span class="box"></span>
-												All </label>
-											</div>
-											<div class="md-radio">
-												<input type="radio" id="radio56" name="level" class="md-radiobtn" checked="">
-												<label for="radio56">
-												<span class="inc"></span>
-												<span class="check"></span>
-												<span class="box"></span>
-												Junior </label>
-											</div>
-											<div class="md-radio">
-												<input type="radio" id="radio57" name="level" class="md-radiobtn" >
-												<label for="radio57">
-												<span></span>
-												<span class="check"></span>
-												<span class="box"></span>
-												Senior </label>
-											</div>
-											<div class="md-radio">
-												<input type="radio" id="radio58" name="level" class="md-radiobtn">
-												<label for="radio58">
-												<span></span>
-												<span class="check"></span>
-												<span class="box"></span>
-												Manager </label>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>							
 							<div class="text-right">
 								<a href="javascript:;" class="btn btn-lg blue text-right">
 									Search <i class="fa fa-search"></i>
