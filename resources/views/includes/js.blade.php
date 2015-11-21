73f4cd15d0859b15328dad986fb119ec91933fbf
cuongduong257 <duongthaicuong@gmail.com>
2015-11-21 13:55:03 +0700

    update code
    update code

--- resources/views/includes/js.blade.php
+++ resources/views/includes/js.blade.php
@@ -7,11 +7,10 @@
 
 <!--Tu trang profile-->
 <script src="assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
-<script src="assets/global/plugins/jquery.blockui.min.js" type="text/javascript"></script>
 
 <script src="assets/admin/layout/scripts/quick-sidebar.js" type="text/javascript"></script>
 <script src="assets/admin/layout/scripts/demo.js" type="text/javascript"></script>
 <script src="assets/admin/pages/scripts/profile.js" type="text/javascript"></script>
 
 
-<!--Tu trang list -->    
+<!--Tu trang list -->
