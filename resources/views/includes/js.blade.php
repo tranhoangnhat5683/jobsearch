5a1a7b2ad40bd39774fbdb9aa41cea9c853d988a
cuongduong257 <duongthaicuong@gmail.com>
2015-11-22 11:54:18 +0700

    Scroll TOp

--- resources/views/includes/js.blade.php
+++ resources/views/includes/js.blade.php
@@ -8,7 +8,6 @@
 
 <!--Tu trang profile-->
 <script src="assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
-
 <script src="assets/admin/layout/scripts/quick-sidebar.js" type="text/javascript"></script>
 <script src="assets/admin/layout/scripts/demo.js" type="text/javascript"></script>
 <script src="assets/admin/pages/scripts/profile.js" type="text/javascript"></script>
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="page-footer-inner">
          2015 &copy; CFF Team (Code For Food)
     </div>
-    <div class="scroll-to-top">
+    <div class="scroll-to-top" style="display: block">
 		<i class="icon-arrow-up"></i>
 	</div>
 </div>
