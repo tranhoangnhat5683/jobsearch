2731f89b77fa0fd85f95b6c9b98060313051254c
cuongduong257 <duongthaicuong@gmail.com>
2015-11-22 11:46:01 +0700

    Update footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,8 +1,8 @@
 <div class="page-footer text-center">
     <div class="page-footer-inner">
-         2015 &copy; CFF Team (Code For Food) <a href="http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes" title="Purchase Metronic just for 27$ and get lifetime updates for free" target="_blank">aa</a>
+         2015 &copy; CFF Team (Code For Food)
     </div>
     <div class="scroll-to-top">
-        <i class="icon-arrow-up"></i>
-    </div>
+		<i class="icon-arrow-up"></i>
+	</div>
 </div>
